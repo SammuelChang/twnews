26d4abc186e23b80777df73e82983b473f86d979
SammuelChang <s23593517844@hotmail.com>
2022-10-15 10:40:15 +0800

    Feat: 新增News API來源 Add: 先行新增資料，以便未來加速推進專案

---
+++ src/utils/api.js
@@ -0,0 +1,11 @@
+const api = {
+    hostname: 'https://newsapi.org/v2',
+    getNews() {
+      return fetch(`${this.hostname}/top-headlines?country=tw&apiKey=f8211c21c46640a8b51ccb7d9ff24c5c`)
+      .then((response) => response.json()
+      );
+    },
+}
+
+
+export default api;
