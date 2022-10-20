8fa429d9a591307061a77f9aaac7dda0df2669f9
SammuelChang <s23593517844@hotmail.com>
2022-10-20 18:18:05 +0800

    Refactor: 將新聞圖牆獨立為元件
    檢視新聞預計有多種模式，為了方便切換，將各個模式獨立為單獨的元件。

---
+++ src/components/BricksView/index.js
@@ -0,0 +1,71 @@
+import styled from 'styled-components/macro'
+
+const Item = styled.li`
+width: 400px;
+height: 450px;
+list-style: none;
+margin: 10px 15px;
+overflow: hidden;
+`
+
+const Photo = styled.div`
+height: 200px;
+width: 100%;
+background: ${props => `url(${props.background}) no-repeat top center`};
+background-size: cover;
+opacity: 0.7;
+transition: .3s ease-in-out;
+cursor: pointer;
+
+&:hover{
+  opacity: 1;
+}
+`
+
+const Title = styled.div`
+font-weight: bold;
+letter-spacing: 2px;
+height: 50px;
+overflow: hidden;
+text-overflow:ellipsis;
+margin: 10px 0;
+`
+
+const Description = styled.div`
+letter-spacing: 2px;
+display: inline-block;
+overflow: hidden;
+text-overflow:ellipsis;
+margin-bottom: 20px;
+`
+
+const PublishtAt = styled.div`
+text-align: right;
+font-size: 0.8rem;
+`
+
+function ellipsis(max, str) {
+    if (typeof str !== 'string') return  '';
+    const maxCount = max;
+    const len = str.length;
+    if (len < maxCount) return str;
+    return str.substring(0, maxCount) + '...';
+  };
+  
+function timeAdjust(str){
+    return str.substring(0, 10)
+}
+
+function BricksView(item){
+    const { news } = item;
+    return (
+    <Item>
+        <Photo background={news.urlToImage}></Photo>
+        <Title>{ellipsis(50, news.title)}</Title>
+        <Description>{ellipsis(80, news.description)}</Description>
+        <PublishtAt>{timeAdjust(news.publishedAt)}</PublishtAt>
+    </Item>
+    )
+}
+
+export default BricksView;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components/macro'
 import { useParams } from "react-router-dom";
 import api from '../../utils/api'
 import { useEffect, useState } from "react";
+import BricksView from '../../components/BricksView'
 
 const Wrapper = styled.div`
 padding: 0 30px;
@@ -16,62 +17,6 @@ flex-wrap: wrap;
 justify-content: center;
 padding: 0;
 `
-
-const Item = styled.li`
-width: 400px;
-height: 450px;
-list-style: none;
-margin: 10px 15px;
-overflow: hidden;
-`
-
-const Photo = styled.div`
-height: 200px;
-width: 100%;
-background: ${props => `url(${props.background}) no-repeat top center`};
-background-size: cover;
-opacity: 0.7;
-transition: .3s ease-in-out;
-cursor: pointer;
-
-&:hover{
-  opacity: 1;
-}
-`
-
-const Title = styled.div`
-font-weight: bold;
-letter-spacing: 2px;
-height: 50px;
-overflow: hidden;
-text-overflow:ellipsis;
-margin: 10px 0;
-`
-
-const Description = styled.div`
-letter-spacing: 2px;
-display: inline-block;
-overflow: hidden;
-text-overflow:ellipsis;
-margin-bottom: 20px;
-`
-
-const PublishtAt = styled.div`
-text-align: right;
-font-size: 0.8rem;
-`
-
-function ellipsis(max, str) {
-  if (typeof str !== 'string') return  '';
-  const maxCount = max;
-  const len = str.length;
-  if (len < maxCount) return str;
-  return str.substring(0, maxCount) + '...';
-};
-
-function timeAdjust(str){
-  return str.substring(0, 10)
-}
 
 function Home() {
   const [news, setNews] = useState([]);
@@ -103,15 +48,9 @@ function Home() {
     <Wrapper>
       <Header/>
       <NewsContainer>
-        {news?.map((item,index) => {
-          return (
-          <Item key={index}>
-            <Photo background={item.urlToImage}></Photo>
-            <Title>{ellipsis(50, item.title)}</Title>
-            <Description>{ellipsis(80, item.description)}</Description>
-            <PublishtAt>{timeAdjust(item.publishedAt)}</PublishtAt>
-          </Item>
-        )})}
+        {news?.map((item,index) => 
+          <BricksView key={index} news={item} />
+        )}
       </NewsContainer>
     </Wrapper>
   );
